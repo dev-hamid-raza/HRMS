627609169e9ec7cdc8081c4298c9dc789504a76f
Hamid Raza <hamidramay2002@gmail.com>
2025-06-27 12:48:27 +0500

    feat: add API response and error handling utilities

---
+++ backend/src/utils/ApiError.ts
@@ -0,0 +1,26 @@
+class ApiError extends Error {
+    statusCode: number
+    data: any
+    message: string
+    success: boolean
+    override stack?: string | undefined
+    constructor(
+        statusCode: number,
+        message = "Something went wrong",
+        errors: unknown[] = [],
+        stack = ""
+    ) {
+        super(message)
+        this.statusCode = statusCode,
+        this.data = null,
+        this.message = message
+        this.success = false
+        this.errors = errors
+
+        if(stack) {
+            this.stack = stack
+        } else {
+            Error.captureStackTrace(this, this.constructor)
+        }
+    }
+}
