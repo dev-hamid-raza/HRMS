2275b4cc1ba5bd7f741672169e17e8b4161e3b42
Hamid Raza <hamidramay2002@gmail.com>
2025-07-01 16:35:38 +0500

    fix: update import statements to include file extensions for consistency

--- backend/src/lib/generateJwtToken.ts
+++ backend/src/lib/generateJwtToken.ts
@@ -1,5 +1,5 @@
-import { User } from "../models/user.model"
-import { ApiError } from "../utils/ApiError"
+import { User } from "../models/user.model.js"
+import { ApiError } from "../utils/ApiError.js"
 
 export const generateAccessTokenAndRefreshToken = async (userId:number) => {
     try {
