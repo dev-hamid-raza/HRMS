651bc5f37588ec87947169bcdb865ebf6feb7c0e
Hamid Raza <hamidramay2002@gmail.com>
2025-07-04 15:49:55 +0500

    refactor: enhance asyncHandler to support generic types for improved type safety

--- backend/src/utils/asyncHandler.ts
+++ backend/src/utils/asyncHandler.ts
@@ -1,7 +1,18 @@
-import { Request, Response, NextFunction, RequestHandler } from "express"
+import { Request, Response, NextFunction, RequestHandler } from "express";
 
-export const asyncHandler = (requestHandler: (req: Request, res: Response, next: NextFunction) => Promise<any>): RequestHandler => {
-    return (req, res, next) => {
-        Promise.resolve(requestHandler(req, res, next)).catch((error) => next(error))
-    }
+export function asyncHandler<
+  P = Record<string, any>,
+  ResBody = any,
+  ReqBody = any,
+  ReqQuery = any
+>(
+  fn: (
+    req: Request<P, ResBody, ReqBody, ReqQuery>,
+    res: Response,
+    next: NextFunction
+  ) => Promise<any>
+): RequestHandler {
+  return (req, res, next) => {
+    fn(req as any, res as any, next).catch(next);
+  };
 }
