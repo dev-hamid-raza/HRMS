a08c32eb2c3c52b40bb8bef7e64639160d404a72
Hamid Raza <hamidramay2002@gmail.com>
2025-07-04 15:50:28 +0500

    feat: add department management functionality with create, delete, and update operations

---
+++ backend/src/controllers/department.controller.ts
@@ -0,0 +1,83 @@
+import { Request, Response } from "express";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { Department } from "../models/department.model.js";
+import { ApiError } from "../utils/ApiError";
+import { ApiResponse } from "../utils/ApiResponse.js";
+
+//! Create Department
+
+export const createDepartment = asyncHandler( async (req:Request<{},{},{departmentName:string}>, res:Response) => {
+    const {departmentName} = req.body
+    if(!departmentName) {
+        throw new ApiError(400,'Department name is required')
+    }
+
+    const department = await Department.create({departmentName})
+
+    const createdDepartment = await Department.findById(department._id)
+
+    if(!createdDepartment) {
+        throw new ApiError(500, "Something went wrong while creating the department")
+    }
+
+    return res
+        .status(201)
+        .json(
+            new ApiResponse(201,{department:createdDepartment},"Department created successfully")
+        )
+})
+
+//! Delete department
+
+export const deleteDepartment = asyncHandler( async (req: Request,res:Response) => {
+    const {id} = req.params
+
+    if(!id) {
+        throw new ApiError(400,'Department id is required')
+    }
+
+    const department = await Department.findById(id)
+
+    if(!department) {
+        throw new ApiError(404, 'Department not found')
+    }
+
+    await department.deleteOne()
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(200,{},'Department deleted successfully')
+        )
+})
+
+//! Update department 
+
+export const updateDepartment = asyncHandler( async(req: Request<{id:string}, {}, {departmentName:string}>, res: Response) => {
+    const {id} = req.params
+    const {departmentName} = req.body
+
+    if(!id) {
+        throw new ApiError(400,'Department id is required')
+    }
+
+    if(!departmentName) {
+        throw new ApiError(400, "Department name is required")
+    }
+
+    const department = await Department.findById(id)
+
+    if(!department) {
+        throw new ApiError(404,'Department not found')
+    }
+
+    department.departmentName = departmentName
+
+    await department.save()
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(200,department,'Department is change successfully')
+        )
+})
