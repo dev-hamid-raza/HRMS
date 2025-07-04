9ac9037d62fa49d8c27a9547685fd3b1f3322d28
Hamid Raza <hamidramay2002@gmail.com>
2025-07-04 15:50:54 +0500

    feat: add designation model and types for managing designations

---
+++ backend/src/models/designation.mode.ts
@@ -0,0 +1,11 @@
+import mongoose, {Model, Schema} from "mongoose";
+import { IDesignation } from "../types/designation.types";
+
+const designationSchema = new Schema<IDesignation>({
+    designationName: {
+        type: String,
+        unique: true
+    }
+})
+
+export const Designation: Model<IDesignation> = mongoose.model<IDesignation>('Designation', designationSchema)
