c5f6d49320e5de650470f5fb746f4c8dbe7ef458
Hamid Raza <hamidramay2002@gmail.com>
2025-07-31 16:22:19 +0500

    add animation and optimized input ui

--- frontend/src/components/ui/input.tsx
+++ frontend/src/components/ui/input.tsx
@@ -1,5 +1,5 @@
 import { cn } from '@/utils/cn';
-import React, { useState } from 'react';
+import React from 'react';
 
 interface InputProps extends React.ComponentProps<'input'> {
 	hasError?: boolean;
@@ -9,52 +9,55 @@ interface InputProps extends React.ComponentProps<'input'> {
 }
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
-	({
-		type,
-		hasError = false,
-		className,
-		placeholder,
-		inputClassName,
-        labelClassName,
-        id,
-		label,
-		value,
-		...props
-	}) => {
-		const [inValue, setValue] = useState(value);
+	(props, ref) => {
+		const {
+			type,
+			hasError = false,
+			className,
+			placeholder,
+			inputClassName,
+			labelClassName,
+			id,
+			label,
+			...rest
+		} = props;
+
 		return (
 			<div className={cn('w-full', className)}>
-                <label 
-                htmlFor={id}
-                className={cn(
-                    'text-text-700 body-primary-sm-medium',
-                    labelClassName
-                )}
-                >{label}</label>
+				{label && (
+					<label
+						htmlFor={id}
+						className={cn(
+							'text-text-700 body-primary-sm-medium',
+							labelClassName
+						)}
+					>
+						{label}
+					</label>
+				)}
 				<input
-                    id={id}
+					id={id}
+					ref={ref}
 					type={type}
 					placeholder={placeholder}
-					value={inValue}
-					onChange={(e) => {
-						setValue(e.target.value);
-						props.onChange?.(e);
-					}}
-                    className={cn(
-                        'flex items-center h-10 rounded-md py-1 px-3 border w-full',
-                        'body-primary-lg-regular text-text-700',
-                        'transition-all ease-in-out duration-200',
-                        'focus-visible:outline-none',
-                        hasError 
-                        ? 'border-danger hover:border-danger-700 focus:border-danger-700 focus:ring-3 focus:ring-danger-100' 
-                        : 'border-border hover:border-border-500 focus:border-border-600 focus:ring-3 focus:ring-border-200',
-                        inputClassName
-                    )}
+					{...rest}
+					className={cn(
+						'flex items-center h-10 rounded-md py-1 px-3 border w-full',
+						'body-primary-lg-regular text-text-700',
+						'transition-all ease-in-out duration-200',
+						'focus-visible:outline-none',
+						hasError
+							? 'border-danger hover:border-danger-700 focus:border-danger-700 focus:ring-3 focus:ring-danger-100'
+							: 'border-border hover:border-border-500 focus:border-border-600 focus:ring-3 focus:ring-border-200',
+						inputClassName
+					)}
 				/>
 			</div>
 		);
 	}
 );
+
+
 Input.displayName = 'Input';
 
 export { Input };
